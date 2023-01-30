1f62251835a0512e23038c9e08a877c43bf9272a
eemim <eemi.mantyla@gmail.com>
2023-01-30 15:42:40 +0200

    ekat lisäykset

---
+++ src/main/java/hh/sof3as3/Bookstore/web/BookController.java
@@ -0,0 +1,15 @@
+package hh.sof3as3.Bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+	
+	@GetMapping("/index")
+	public String getBook() {
+		
+		return "storeindex";
+	}
+
+}
