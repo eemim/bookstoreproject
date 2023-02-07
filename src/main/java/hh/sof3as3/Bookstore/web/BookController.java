a1520750dff8459532bae11f8916af261b93e952
eemim <eemi.mantyla@gmail.com>
2023-02-07 19:41:30 +0200

    Adding listpage

--- src/main/java/hh/sof3as3/Bookstore/web/BookController.java
+++ src/main/java/hh/sof3as3/Bookstore/web/BookController.java
@@ -1,15 +1,24 @@
 package hh.sof3as3.Bookstore.web;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import hh.sof3as3.Bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
 	
-	@GetMapping("/index")
-	public String getBook() {
+	@Autowired
+	private BookRepository bookRepository;
+	
+	@GetMapping("/booklist")
+	public String getBooks(Model model) {
 		
-		return "storeindex";
+		model.addAttribute("books", bookRepository.findAll());
+		
+		return "booklist";
 	}
 
 }
