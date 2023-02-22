153375bbddc45d3abc340c8bc310c2041c7879b2
eemim <eemi.mantyla@gmail.com>
2023-02-22 15:10:59 +0200

    Category of Book

--- src/main/java/hh/sof3as3/Bookstore/BookstoreApplication.java
+++ src/main/java/hh/sof3as3/Bookstore/BookstoreApplication.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Bean;
 
 import hh.sof3as3.Bookstore.domain.Book;
 import hh.sof3as3.Bookstore.domain.BookRepository;
+import hh.sof3as3.Bookstore.domain.Category;
+import hh.sof3as3.Bookstore.domain.CategoryRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -18,9 +20,21 @@ public class BookstoreApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner examples(BookRepository bookRepository) {
+	public CommandLineRunner examples(BookRepository bookRepository, CategoryRepository categoryRepository) {
 		 
 		return (args) -> {
+			Category category1 = new Category("Fantasy");
+			Category category2 = new Category("Children");
+			Category category3 = new Category("Non-fiction");
+			categoryRepository.save(category1);
+			categoryRepository.save(category2);
+			categoryRepository.save(category3);
+			
+			List<Category> categories = (List<Category>) categoryRepository.findAll();
+			for (Category category : categories) {
+				System.out.println(category.toString());
+			}
+			
 			Book book1 = new Book("A.A. Milne", "Nalle Puh rakentaa talon", 1928, "1232323-22", 19.95);
 			Book book2 = new Book ("J.R.R. Tolkien", "The Lord of the Rings", 1954, "3232321-21", 29.95);
 			bookRepository.save(book1);
---
+++ src/main/java/hh/sof3as3/Bookstore/domain/Category.java
@@ -0,0 +1,68 @@
+package hh.sof3as3.Bookstore.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Category {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long categoryid;
+	private String name;
+	
+	
+	public Category() {
+		super();
+		this.categoryid = null;
+		this.name = null;
+	}
+	
+
+
+	public Category(String name) {
+		super();
+		this.name = name;
+	}
+
+
+
+	public Category(Long categoryid, String name) {
+		super();
+		this.categoryid = categoryid;
+		this.name = name;
+	}
+
+
+	public Long getCategoryid() {
+		return categoryid;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setCategoryid(Long categoryid) {
+		this.categoryid = categoryid;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Category [categoryid=" + categoryid + ", name=" + name + "]";
+	}
+	
+	
+	
+	
+
+}
