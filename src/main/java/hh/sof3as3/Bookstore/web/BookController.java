319159c92440c5b708409fe92faced859b2e8fc7
eemim <eemi.mantyla@gmail.com>
2023-03-06 17:19:27 +0200

    Security part1

--- src/main/java/hh/sof3as3/Bookstore/web/BookController.java
+++ src/main/java/hh/sof3as3/Bookstore/web/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -61,6 +62,7 @@ public class BookController {
 	}
 
 	@GetMapping("/delete/{id}")
+	@PreAuthorize("hasRole('ADMIN')")
 	public String deleteBook(@PathVariable("id") Long id) {
 		bookRepository.deleteById(id);
 		return "redirect:/booklist";
